c47e98bc9accf05242bf27ce79e684009864151a
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2022-02-09 00:50:53 +0300

    Финализация1

--- Занятие6/MainWindow.xaml.cs
+++ Занятие6/MainWindow.xaml.cs
@@ -15,14 +15,70 @@ using System.Windows.Shapes;
 
 namespace Занятие6
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    enum Fallout
     {
-        public MainWindow()
+        sunny, 
+        cloudy, 
+        raining, 
+        snowing
+    }
+
+
+     class WeatherControl : DependencyObject
+    {
+
+        public string winddirection;
+        public int windspeed;
+        public Fallout fallout;
+
+        public WeatherControl(string winddirection, int windspeed, Fallout fallout)
         {
-            InitializeComponent();
+            this.winddirection = winddirection;
+            this.windspeed = windspeed;
+            this.fallout = fallout;
+        }
+
+        public static readonly DependencyProperty TempProperty;
+        public int Temp
+        {
+            get => (int)GetValue(TempProperty);
+            set => SetValue(TempProperty, value);
+        }
+
+        static WeatherControl()
+        {
+            ValidateValueCallback ValidateTemp = null;
+            CoerceValueCallback CoerceTemp = null;
+            TempProperty = DependencyProperty.Register(
+                nameof(Temp),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemp)),
+                new ValidateValueCallback(ValidateTemp));
+        }
+
+        private static bool ValidateTemp(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+                return true;
+            else
+                return false;
+        }
+
+        private static object CoerceTemp(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= 0)
+                return v;
+            else
+                return 0;
         }
     }
+
 }
